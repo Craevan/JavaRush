86d86da0df0c1fea9ab743e31b452f0ddc3c0f66
Crevan <a.siverskii@gmail.com>
2021-12-10 20:55:02 +0300

    add task 1318-1322

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task13.task1318;
+
+/* 
+Следующий месяц, пожалуйста
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+    }
+
+    public static Month getNextMonth(Month month) {
+        //напишите тут ваш код
+        Month[] months = Month.values();
+        int tmp = month.ordinal() + 1;
+        if (tmp > 11)
+            return months[0];
+        return months[tmp];
+    }
+}
