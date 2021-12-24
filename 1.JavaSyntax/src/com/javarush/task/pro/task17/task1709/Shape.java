5d8f4d27670165916140a587f149b47267596584
Crevan <a.siverskii@gmail.com>
2021-12-24 13:25:03 +0300

    add tasks 1709 - 1717 Lvl 9 is done!

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+public abstract class Shape {
+    //напишите тут ваш код
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+}
