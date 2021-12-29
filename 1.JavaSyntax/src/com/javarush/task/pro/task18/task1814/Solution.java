1600393a215c02117583a2a57f6a34971134b1bb
Crevan <a.siverskii@gmail.com>
2021-12-29 15:19:53 +0300

    add tasks 1808 - 1816

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1814/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task18.task1814;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/* 
+Без повторов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var words = new ArrayList<String>();
+        Collections.addAll(words, "чтобы", "стать", "программистом", "нужно", "программировать",
+                "а", "чтобы", "программировать", "нужно", "учиться");
+
+        Stream<String> distinctWords = getDistinctWords(words);
+        distinctWords.forEach(System.out::println);
+    }
+
+    public static Stream<String> getDistinctWords(ArrayList<String> words) {
+        //напишите тут ваш код
+        final List<String> uniqueWords = words.stream().distinct().collect(Collectors.toList());
+        return uniqueWords.stream();
+    }
+}
