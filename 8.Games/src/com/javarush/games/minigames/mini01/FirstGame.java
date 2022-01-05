7f5b3345aa264a0efdd00f8392ac326e9a6c1c20
Crevan <a.siverskii@gmail.com>
2022-01-05 22:39:18 +0300

    add tasks miniGames 01, 02

---
+++ 8.Games/src/com/javarush/games/minigames/mini01/FirstGame.java
@@ -0,0 +1,14 @@
+package com.javarush.games.minigames.mini01;
+
+import com.javarush.engine.cell.Game;
+
+public class FirstGame extends Game {
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
