cccf873ffc26153dfbbd9c556dce08fcd9cc2163
a.siverskii <a.siverskii@gmail.com>
2022-01-10 19:54:50 +0300

    add tasks lvl 2 JavaCoreQuest

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1;
+        print(a);
+        print((Integer) 1);
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(int a) {}
+
+    static void print(Integer a) {}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(String s) {}
+    static void print(String s, int a) {}
+    static void print(int a) {}
+    static void print(int a, int b) {}
+    static void print() {}
+}
