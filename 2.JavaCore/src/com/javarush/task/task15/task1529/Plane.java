4fd8ce98bebb29febf4e305df58696a4de560395
a.siverskii <a.siverskii@gmail.com>
2022-01-22 18:00:38 +0300

    add tasks 1529 lvl 5 JavaCoreQuest

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+
+    private final int countOfPass;
+
+    public Plane(int countOfPass) {
+        this.countOfPass = countOfPass;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
