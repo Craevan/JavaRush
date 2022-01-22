b5e90f2cc6f61355628c39fbf87a54618debbc99
a.siverskii <a.siverskii@gmail.com>
2022-01-22 17:59:50 +0300

    add tasks 1523 lvl 5 JavaCoreQuest

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    public SubSolution(String s) {
+        super(s);
+    }
+
+    protected SubSolution(byte b) {
+        super(b);
+    }
+
+    SubSolution() {
+    }
+}
