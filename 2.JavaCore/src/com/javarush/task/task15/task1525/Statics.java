5da7ca2a57ec33b3c42393582220eb1101343908
a.siverskii <a.siverskii@gmail.com>
2022-01-22 18:00:05 +0300

    add tasks 1525 lvl 5 JavaCoreQuest

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    /* add the path to your source file here */
+    public static String FILE_NAME =
+            "/home/user/Documents/Repositories/GitHub/JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1525/test.txt";
+}
