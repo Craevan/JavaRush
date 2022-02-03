a2898375e4cdc007bd9526f67da96a55fc8baf50
a.siverskii <a.siverskii@gmail.com>
2022-02-03 16:05:39 +0300

    add task 1814 lvl 8 JavaCoreQuest Solved

--- 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
@@ -3,22 +3,21 @@ package com.javarush.task.task18.task1814;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-
 /* 
 UnsupportedFileName
 */
 
 public class TxtInputStream extends FileInputStream {
 
-    public TxtInputStream(String fileName) throws IOException, UnsupportedFileNameException {
-        if (verify(fileName)) {
-            super(new);
-        }
-        else {
+    public TxtInputStream(String fileName) throws UnsupportedFileNameException, IOException {
+        super(fileName);
+        if (!verify(fileName)) {
             super.close();
             throw new UnsupportedFileNameException();
         }
     }
+
+
 
     private boolean verify(String text) {
         String[] strings = text.split("\\.");
