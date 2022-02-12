f98a639beb394cb32af811b929563303d1cf00a8
Crevan <a.siverskii@gmail.com>
2022-02-12 11:56:17 +0300

    add task 1917 lvl 10 JavaCoreQuest

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task19.task1917;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/* 
+Свой FileWriter
+*/
+
+public class FileConsoleWriter {
+    private final FileWriter fileWriter;
+
+    public FileConsoleWriter(String name) throws IOException {
+        fileWriter = new FileWriter(name);
+    }
+
+    public FileConsoleWriter(String name, boolean append) throws IOException {
+        fileWriter = new FileWriter(name, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        fileWriter = new FileWriter(fd);
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        System.out.println(new String(cbuf).substring(off, off + len));
+    }
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println((char) c);
+    }
+
+    public void write(String s) throws IOException {
+        fileWriter.write(s);
+        System.out.println(s);
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(str.substring(off, off + len));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(new String(cbuf));
+    }
+
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
