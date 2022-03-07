fb2bdbf50587781e264ffdaab08d0819a456c2b1
a.siverskii <a.siverskii@gmail.com>
2022-03-07 15:54:04 +0300

    add task 3111 lvl 1 JavaCollectionsQuest

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task31.task3111;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchFileVisitor extends SimpleFileVisitor<Path> {
+    private String partOfName;
+    private String partOfContent;
+
+    private int minSize;
+    private int maxSize;
+
+    private List<Path> foundFiles = new ArrayList<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (partOfName != null && !file.getFileName().toString().contains(partOfName)) return FileVisitResult.CONTINUE;
+        byte[] content = Files.readAllBytes(file); // размер файла: content.length
+        if ((maxSize > 0 && content.length > maxSize) || (minSize > 0 && content.length < minSize)) return FileVisitResult.CONTINUE;
+
+        // check contents of file
+        if (partOfContent != null && !partOfContent.isEmpty()) {
+            String contentString = new String(content);
+            if (!contentString.contains(partOfContent)) return FileVisitResult.CONTINUE;
+        }
+
+        foundFiles.add(file);
+
+        return super.visitFile(file, attrs);
+    }
+
+    public void setPartOfName(String partOfName) {
+        this.partOfName = partOfName;
+    }
+
+    public void setPartOfContent(String partOfContent) {
+        this.partOfContent = partOfContent;
+    }
+
+    public void setMinSize(int i) {
+        this.minSize = i;    }
+
+    public void setMaxSize(int i) {
+        this.maxSize = i;
+    }
+
+    public List<Path> getFoundFiles() {
+        return foundFiles;
+    }
+}
