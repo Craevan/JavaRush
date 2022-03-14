e7e6d1606eaaf2b5bc40fe995e1d34989633b351
a.siverskii <a.siverskii@gmail.com>
2022-03-14 21:23:25 +0300

    add task 2502 lvl 5 JavaMultithreadingQuest

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/* 
+Машину на СТО не повезем!
+*/
+
+public class Solution {
+    public enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+            String[] str = loadWheelNamesFromDB();
+            this.wheels = new ArrayList<>();
+
+            for (String s : str) {
+                this.wheels.add(Wheel.valueOf(s));
+            }
+            if (wheels.size() != 4)
+                throw new IllegalArgumentException();
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
