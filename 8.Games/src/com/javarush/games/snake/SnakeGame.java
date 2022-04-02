b65ed005a1944e4756834a871c66c04e92c041d2
a.siverskii <a.siverskii@gmail.com>
2022-04-02 23:08:25 +0300

    starting SnakeGame

---
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -0,0 +1,27 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+
+public class SnakeGame extends Game  {
+
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellColor(i, j, Color.DARKSEAGREEN);
+            }
+        }
+    }
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+}
