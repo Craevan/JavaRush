b0161643fbb185a9d355c1de6fd0f2d336bad401
a.siverskii <a.siverskii@gmail.com>
2022-05-15 17:09:25 +0300

    start MoonLanderGame

---
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -0,0 +1,92 @@
+package com.javarush.games.moonlander;
+
+import com.javarush.engine.cell.*;
+
+public class MoonLanderGame extends Game {
+
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
+
+    private Rocket rocket;
+    private GameObject landscape;
+    private boolean isUpPressed;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
+
+    private void drawScene() {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellColor(i, j, Color.BEIGE);
+            }
+        }
+        landscape.draw(this);
+        rocket.draw(this);
+    }
+    
+    private void createGame() {
+        isUpPressed  = false;
+        isLeftPressed = false;
+        isRightPressed = false;
+        createGameObjects();
+        setTurnTimer(50);
+        drawScene();
+    }
+
+    private void createGameObjects() {
+        rocket = new Rocket(WIDTH / 2.0, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        showGrid(false);
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    @Override
+    public void onTurn(int step) {
+//        super.onTurn(step);
+        rocket.move(isUpPressed, isLeftPressed, isRightPressed);
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key) {
+            case UP:
+                isUpPressed = true;
+                break;
+            case LEFT:
+                isLeftPressed = true;
+                isRightPressed = false;
+                break;
+            case RIGHT:
+                isRightPressed = true;
+                isLeftPressed = false;
+                break;
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        switch (key) {
+            case UP:
+                isUpPressed = false;
+                break;
+            case LEFT:
+                isLeftPressed = false;
+                break;
+            case RIGHT:
+                isRightPressed = false;
+                break;
+        }
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x > 0 && x < WIDTH && y > 0 && y < HEIGHT)
+            super.setCellColor(x, y, color);
+    }
+}
---
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -0,0 +1,31 @@
+package com.javarush.games.moonlander;
+
+public class Rocket extends GameObject {
+
+    private double speedY = 0;
+    private double speedX = 0;
+    private double boost = 0.05;
+
+    public Rocket(double x, double y) {
+        super(x, y, ShapeMatrix.ROCKET);
+    }
+
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed)
+            speedY -= boost;
+        else
+            speedY += boost;
+
+        y += speedY;
+
+        if (isLeftPressed || isRightPressed) {
+            if (isLeftPressed)
+                speedX -= boost;
+            if (isRightPressed)
+                speedX += boost;
+
+            x += speedX;
+        }
+    }
+
+}
