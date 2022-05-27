5547bc2f9a511d88c063503e5c2bada5d8185e57
a.siverskii <a.siverskii@gmail.com>
2022-05-27 21:27:24 +0300

    add task 3804 lvl 8 JavaCollectionsQuest

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3804/ExceptionFactory.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task38.task3804;
+
+public class ExceptionFactory {
+    public static Throwable createException(Enum e) {
+        if (e == null) {
+            return new IllegalArgumentException();
+        }
+        String name = e.name().toLowerCase().replaceAll("_", " ");
+        String firstCapitalLetter = name.substring(0, 1).toUpperCase();
+        String message = firstCapitalLetter + name.substring(1);
+
+        switch (e.getClass().getSimpleName()) {
+            case ("ApplicationExceptionMessage") :
+                return new Exception(message);
+            case ("DatabaseExceptionMessage") :
+                return new RuntimeException(message);
+            case ("UserExceptionMessage") :
+                return new Error(message);
+            default:
+                return new IllegalArgumentException();
+        }
+    }
+}
