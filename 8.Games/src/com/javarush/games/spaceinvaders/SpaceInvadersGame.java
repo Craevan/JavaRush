46e8039de2dde12faef67f8c0f249e0f242d3115
Crevan <a.siverskii@gmail.com>
2023-01-31 18:46:29 +0100

    [add] solution for "SpaceInvadersGame" #2

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -6,8 +6,22 @@ public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
 
+    private void drawField() {
+
+    }
+
+    private void drawScene() {
+        drawField();
+    }
+
+    private void createGame() {
+        drawScene();
+    }
+
+
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
     }
 }
