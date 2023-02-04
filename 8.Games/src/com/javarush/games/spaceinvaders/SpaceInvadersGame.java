2d9629ad6dd463398e8f18089fc6ca71e1ca6f4b
Crevan <a.siverskii@gmail.com>
2023-02-04 20:06:16 +0100

    [add] solution for "SpaceInvadersGame" #8

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,6 +1,7 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.EnemyFleet;
 import com.javarush.games.spaceinvaders.gameobjects.Star;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class SpaceInvadersGame extends Game {
     public static final int HEIGHT = 64;
 
     private List<Star> stars;
+    private EnemyFleet enemyFleet;
 
     private void drawField() {
         for (int y = 0; y < HEIGHT; y++) {
@@ -27,11 +29,14 @@ public class SpaceInvadersGame extends Game {
 
     private void drawScene() {
         drawField();
+        enemyFleet.draw(this);
     }
 
     private void createGame() {
         createStars();
+        enemyFleet = new EnemyFleet();
         drawScene();
+        setTurnTimer(40);
     }
 
     private void createStars() {
@@ -47,5 +52,10 @@ public class SpaceInvadersGame extends Game {
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
+    }
+
+    @Override
+    public void onTurn(int step) {
+        drawScene();
     }
 }
