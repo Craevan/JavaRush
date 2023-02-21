c9ea7b505a3a0e8e41865dc4d147adf3373e2e8c
Crevan <a.siverskii@gmail.com>
2023-02-21 22:56:08 +0300

    [add] "Racer" solution #5

---
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -0,0 +1,27 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+    public int x;
+    public int y;
+    public int[][] matrix;
+    public int width;
+    public int height;
+
+    public GameObject(final int x, final int y, final int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+        this.width = matrix[0].length;
+        this.height = matrix.length;
+    }
+
+    public void draw(final Game game) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                game.setCellColor(x + i, y + j, Color.NONE);
+            }
+        }
+    }
+}
