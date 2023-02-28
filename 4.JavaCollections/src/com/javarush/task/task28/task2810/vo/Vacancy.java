10d5057c94ba6e40b8c987f7d93d46417c9147cf
Crevan <a.siverskii@gmail.com>
2023-02-28 23:27:14 +0300

    [add] solution for task 2810 #3

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task28.task2810.vo;
+
+public class Vacancy {
+    private String title;
+    private String salary;
+    private String city;
+    private String companyName;
+    private String siteName;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(final String salary) {
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(final String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(final String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+}
