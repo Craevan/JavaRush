932d0c78b5721ddd99db09049f4675c567e86eb8
Crevan <a.siverskii@gmail.com>
2023-03-03 21:19:34 +0300

    [add] solution for task 2810 #8

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -3,6 +3,8 @@ package com.javarush.task.task28.task2810.model;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,13 +13,45 @@ import java.util.List;
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
 
+    protected Document getDocument(String searchString, int page) throws IOException {
+        return Jsoup.connect(String.format(URL_FORMAT, searchString, page))
+                .userAgent("Mozilla/5.0")
+                .referrer("https://hh.ru/")
+                .get();
+    }
+
     @Override
     public List<Vacancy> getVacancies(final String searchString) {
+        final List<Vacancy> vacancies = new ArrayList<>();
+        int page = 0;
         try {
-            Document document = Jsoup.connect(URL_FORMAT).userAgent("Mozilla/5.0").referrer("https://spb.hh.ru/search/vacancy?text=java").get();
+            do {
+                final Document doc = getDocument(searchString, page);
+                final Elements vacanciesList = doc.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+                if (vacanciesList.isEmpty()) {
+                    break;
+                }
+                for (Element element : vacanciesList) {
+                    Elements links = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title");
+                    Elements locations = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address");
+                    Elements companyName = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer");
+                    Elements salary = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation");
+
+                    Vacancy vacancy = new Vacancy();
+                    vacancy.setSiteName("hh.ru");
+                    vacancy.setTitle(links.get(0).text());
+                    vacancy.setUrl(links.get(0).attr("href"));
+                    vacancy.setCity(locations.get(0).text());
+                    vacancy.setCompanyName(companyName.get(0).text());
+                    vacancy.setSalary(salary.size() > 0 ? salary.get(0).text() : "");
+
+                    vacancies.add(vacancy);
+                }
+                page++;
+            } while (true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ArrayList<>();
+        return vacancies;
     }
 }
