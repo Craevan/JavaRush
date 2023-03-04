9886a3a76d0d6f6e81e24fe4279ce36d7b9b0741
Crevan <a.siverskii@gmail.com>
2023-03-04 22:36:18 +0300

    [add] solution for task 2810 #15

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,6 +2,10 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.*;
 import java.util.List;
@@ -17,8 +21,47 @@ public class HtmlView implements View {
         controller.onCitySelect("Odessa");
     }
 
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
+    }
+
     private String getUpdatedFileContent(final List<Vacancy> vacancies) {
-        return ""; //STUB
+        try {
+            Document doc = getDocument();
+            Elements elements = doc.getElementsByClass("template");
+            Element copy = elements.clone().removeAttr("style").removeClass("template").get(0);
+            Elements oldVacancies = doc.getElementsByClass("vacancy");
+
+            oldVacancies.forEach(vacancy -> {
+                if (!vacancy.hasClass("template")) {
+                    vacancy.remove();
+                }
+            });
+
+            vacancies.forEach(vacancy -> {
+                Element vacEl = copy.clone();
+
+                Element vacLink = vacEl.getElementsByAttribute("href").get(0);
+                vacLink.appendText(vacancy.getTitle());
+                vacLink.attr("href", vacancy.getUrl());
+
+                Element city = vacEl.getElementsByClass("city").get(0);
+                city.appendText(vacancy.getCity());
+
+                Element company = vacEl.getElementsByClass("companyName").get(0);
+                company.appendText(vacancy.getCompanyName());
+
+                Element salary = vacEl.getElementsByClass("salary").get(0);
+                salary.appendText(vacancy.getSalary());
+
+                elements.before(vacEl.outerHtml());
+            });
+
+            return doc.html();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Some exception occurred";
     }
 
     private void updateFile(final String fileBody) {
