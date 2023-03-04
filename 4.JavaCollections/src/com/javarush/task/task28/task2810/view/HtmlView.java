55ec0e14df58a6e2529ff01e8c21cf52b74d493d
Crevan <a.siverskii@gmail.com>
2023-03-04 21:24:43 +0300

    [add] solution for task 2810 #13

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -6,17 +6,27 @@ import com.javarush.task.task28.task2810.vo.Vacancy;
 import java.util.List;
 
 public class HtmlView implements View {
+    private final String projectSourceFolder = "./4.JavaCollections/src/";
+    private final String packages = this.getClass().getPackage().getName().replaceAll("\\.", "/");
+    private final String fileName = "vacancies.html";
+    private final String filePath = projectSourceFolder + packages + "/" + fileName;
     private Controller controller;
-
-
 
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Odessa");
     }
 
+    private String getUpdatedFileContent(final List<Vacancy> vacancies) {
+        return ""; //STUB
+    }
+
+    private void updateFile(final String fileName) {
+
+    }
+
     @Override
     public void update(final List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        updateFile(getUpdatedFileContent(vacancies));
     }
 
     @Override
