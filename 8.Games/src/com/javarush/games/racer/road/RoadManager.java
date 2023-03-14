2a42821732282d0499ff48776b7590dcdec08bad
Crevan <a.siverskii@gmail.com>
2023-03-14 23:27:40 +0300

    [add] solution for Racer game #12

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -1,15 +1,32 @@
 package com.javarush.games.racer.road;
 
+import com.javarush.engine.cell.Game;
 import com.javarush.games.racer.RacerGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoadManager {
+    private static final int FIRST_LANE_POSITION = 16;
+    private static final int FOURTH_LANE_POSITION = 44;
     public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH;
     public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
+
+    private List<RoadObject> items = new ArrayList<>();
 
     private RoadObject createRoadObject(final RoadObjectType type, final int x, final int y) {
         if (type == RoadObjectType.THORN) {
             return new Thorn(x, y);
         }
         return null;
+    }
+
+    private void addRoadObject(RoadObjectType type, Game game) {
+        int x = game.getRandomNumber(FIRST_LANE_POSITION, FOURTH_LANE_POSITION);
+        int y = -1 * RoadObject.getHeight(type);
+        RoadObject roadObject = createRoadObject(type, x, y);
+        if (roadObject != null) {
+            items.add(roadObject);
+        }
     }
 }
